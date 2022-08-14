653be056a63da2095bc6f715394e42477899ec62
Konstantin Ligus <kostya1989com@gmail.com>
2022-08-14 20:38:27 +0300

    delete log

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,7 +12,6 @@ export class App extends Component {
   };
 
   onLeaveFeedback = ({ target: { name } }) => {
-    console.log(name);
     this.setState(prevState => ({ [name]: prevState[name] + 1 }));
   };
   countTotalFeedback = () => {
